30f1bf53d91f7a9a2cae6dc1a2b876a3a7a4d534
Radu mardari <radumardari89@gmail.com>
2024-03-05 13:19:57 +0300

    tru commt

--- src/Car.java
+++ src/Car.java
@@ -2,5 +2,6 @@ public class Car {
     public static void build() {
         System.out.println("you must build a new car");
         System.out.println("b");
+        System.out.println("c");
     }
 }
